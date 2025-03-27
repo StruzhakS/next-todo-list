c53c335b6c607635525d592c612b5a5df8d2964e
Serhii <StruzhakS@gmail.com>
2025-03-27 12:03:09 +0200

    add loader

--- src/components/allTodos/AllTodos.jsx
+++ src/components/allTodos/AllTodos.jsx
@@ -7,19 +7,33 @@ import QuantityOfTodos from "../quantityOfTodos/QuantityOfTodos";
 import { getToDoItems } from "@/lib/api";
 import AddTodo from "../addTodo/AddTodo";
 import s from "./AllTodos.module.css";
+import Loader from "../loader/Loader";
 
 const AllTodos = () => {
   const [todos, setTodos] = useState([]);
   const [limit, setLimit] = useState(10);
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null); 
 
   useEffect(() => {
     const getAllTodos = async () => {
-      const data = await getToDoItems(limit);
-      setTodos(data);
+      setIsLoading(true);
+      setError(null);
+
+      try {
+        const data = await getToDoItems(limit);
+        setTodos(data);
+      } catch (err) {
+        setError("Failed to fetch todos. Please try again.");
+      } finally {
+        setIsLoading(false);
+      }
     };
     getAllTodos();
   }, [limit]);
+
+  console.log(isLoading);
 
   return (
     <div className={s.container}>
@@ -30,11 +44,17 @@ const AllTodos = () => {
         todos={todos}
         setTodos={setTodos}
       />
-      <ul className={s.todoList}>
-        {todos.map((todo, idx) => (
-          <Todo key={idx} todo={todo} todos={todos} setTodos={setTodos} />
-        ))}
-      </ul>
+
+      {isLoading && <Loader/>}
+      {error && <p style={{ color: "red" }}>{error}</p>}
+
+      {!isLoading && !error && (
+        <ul className={s.todoList}>
+          {todos.map((todo, idx) => (
+            <Todo key={idx} todo={todo} todos={todos} setTodos={setTodos} />
+          ))}
+        </ul>
+      )}
     </div>
   );
 };
